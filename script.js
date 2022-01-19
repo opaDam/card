95976761138c8259786abeeb73bc6543390d7804
opaDam <54212616+opaDam@users.noreply.github.com>
2022-01-19 17:45:26 +0100

    19-01-2022

--- script.js
+++ script.js
@@ -5,26 +5,31 @@ let x
 let song = "off"
 let dump
 let e
+let test
 
 for (i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function (e) {
-        e.target.classList.toggle("active")
+        test = e.target.classList[1]
+        console.log(test);
+
+        e.target.classList.add("active")
         dump = e.target
-        console.log(e, "-", e);
+        dump.innerText = "Stop Music"
+        console.log("eventListener");
     })
 }
 
 function play_music(x) {
-    if (dump) dump.classList.remove("active"), song = "off"
-    console.log(x)
-    song = `https://opadam.github.io/map/map/${x}`
-    audioPlayer.src = song
+    if (dump) dump.classList.remove("active")
+    dataSong = `https://opadam.github.io/map/map/${x}`
+    audioPlayer.src = dataSong
     audioPlayer.play()
     song = "on"
 }
 
 function stop() {
-    if (dump) dump.classList.remove("active"), song = "off"
+    if (dump) dump.classList.remove("active")
+    dump.innerText = "Play Music"
     audioPlayer.pause()
     audioPlayer.src = ""
 }
